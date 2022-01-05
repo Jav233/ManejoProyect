bb8a29de811ff3e0b86504f53086783a31add8a3
Jav233 <zamoraxavier233@gmail.com>
2022-01-05 15:15:51 -0500

    PROYEC-1 Creacion y modificacion de clases.

--- src/manejodesoftware/Usuarios.java
+++ src/manejodesoftware/Usuarios.java
@@ -5,7 +5,27 @@
  */
 package manejodesoftware;
 
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 
 public class Usuarios {
+    public static Scanner sc = new Scanner(System.in);
     
+    public boolean sesionAdministrador() {
+        return false;
+    }
+    
+    public void ingresarUsuario() throws IOException {
+        
+    }
+    
+    public Usuarios sesionUsuario() throws IOException {
+        LinkedList<Usuarios> usuarios;
+        String correo, contraseña;
+
+        System.out.printf("\n%15s\n","Error: El usuario es incorrecto");
+        return null;
+    }
 }
